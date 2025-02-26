f2c3b920de6fe79b33aff2a79500da802ed004ff
euskate <euskate07@gmail.com>
2025-02-26 17:35:36 +0900

    URL 복사버튼 만들기

--- src/components/testResult/ShareButtonGroup.jsx
+++ src/components/testResult/ShareButtonGroup.jsx
@@ -5,6 +5,8 @@ import {
   XIcon,
 } from "react-share";
 import { base_url } from "../../App";
+import styles from "./shareButtonGroup.module.css";
+import { useCopyToClipboard } from "@uidotdev/usehooks";
 
 export const ShareButtonGroup = ({
   testParam,
@@ -13,19 +15,35 @@ export const ShareButtonGroup = ({
 }) => {
   const share_url = `${base_url}/${testParam}/result/${resultParam}`;
   const mainTitle = renderTestInfo?.info?.mainTitle;
+  const [copiedText, copy] = useCopyToClipboard();
   return (
     <>
-      <h1>친구에게 공유하기</h1>
-      <FacebookShareButton url={share_url} hashtag={`#${mainTitle}`}>
-        <FacebookIcon round={true} size={48} />
-      </FacebookShareButton>
-      <TwitterShareButton
-        title={mainTitle}
-        url={share_url}
-        hashtags={[mainTitle]}
-      >
-        <XIcon round={true} size={48} className={``} />
-      </TwitterShareButton>
+      <h3>친구에게 공유하기</h3>
+      <div className={styles.shareButtonDiv}>
+        <FacebookShareButton url={share_url} hashtag={`#${mainTitle}`}>
+          <FacebookIcon
+            className={styles.socialMediaIcon}
+            round={true}
+            size={48}
+          />
+        </FacebookShareButton>
+        <TwitterShareButton
+          title={mainTitle}
+          url={share_url}
+          hashtags={[mainTitle]}
+        >
+          <XIcon className={styles.socialMediaIcon} round={true} size={48} />
+        </TwitterShareButton>
+        <button
+          className={styles.urlShareButton}
+          onClick={() => {
+            copy(share_url);
+            alert("URL이 복사 되었습니다.");
+          }}
+        >
+          URL
+        </button>
+      </div>
     </>
   );
 };
