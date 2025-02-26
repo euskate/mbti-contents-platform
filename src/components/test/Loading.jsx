a9169513acf1f056d008e58371eaf22de5d2b715
euskate <euskate07@gmail.com>
2025-02-26 14:10:19 +0900

    로딩페이지 만들기1

--- src/components/test/Loading.jsx
+++ src/components/test/Loading.jsx
@@ -1,5 +1,27 @@
+import Lottie from "react-lottie";
+import * as animationData from "../../assets/loading-animation.json";
+
 export const Loading = () => {
-  return <>Loading</>;
+  const defaultOption = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData.default,
+    renderSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <Lottie
+      options={defaultOption}
+      height={250}
+      width={250}
+      style={{ marginTop: "10rem" }}
+    />
+  );
 };
 
 export default Loading;
+
+// Loading 애니메이션
+// mbtiScore 계산 -> result MBTI Type ex. "ENFP"
+// N초 후 -> Result 페이지로 라우팅
