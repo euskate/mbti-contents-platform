8720a57ac676482aa01118e2a50170d0ba4428c2
euskate <euskate07@gmail.com>
2025-02-26 12:56:37 +0900

    Quiz 페이지 만들기 2

--- src/components/test/Quiz.jsx
+++ src/components/test/Quiz.jsx
@@ -2,18 +2,29 @@ import { useEffect, useState } from "react";
 
 export const Quiz = ({ setMode, questions, mbtiScore, setMbtiScore }) => {
   const [questionNum, setQuestionNum] = useState(0);
-  const onOptionClick = () => {
+  const onOptionClick = (type) => {
+    mbtiScore[type] += 1;
+    setMbtiScore({ ...mbtiScore });
     setQuestionNum((prev) => prev + 1);
   };
 
-  useEffect(() => {}, [questionNum]);
+  useEffect(() => {
+    if (questionNum === questions.length) {
+      setMode("loading");
+    }
+  }, [questionNum, questions.length, setMode]);
+
   return (
     <>
       <h3>{questions[questionNum]?.question}</h3>
-      <button onClick={onOptionClick}>
+      <button
+        onClick={() => onOptionClick(questions[questionNum]?.answers[0]?.type)}
+      >
         {questions[questionNum]?.answers[0]?.content}
       </button>
-      <button onClick={onOptionClick}>
+      <button
+        onClick={() => onOptionClick(questions[questionNum]?.answers[0]?.type)}
+      >
         {questions[questionNum]?.answers[1]?.content}
       </button>
       <div>프로그레스바</div>
