ae3e60a2b548ead8d238cdf755a2e2fef1c0b2ca
euskate <euskate07@gmail.com>
2025-03-04 13:29:50 +0900

    AWS 호스팅 사이트를 위한 CI/CD (github action workflow)

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+// CI/CD
+// 1. 코드작성 수정
+// 2. GitHub Push
+// 3. 프로젝트 빌드 -> 빌드파일
+// 4. S3에 빌드파일 업로드
+// 5. Cloud Front Invalidation
+
+// 3~5fmf Github Action을 통해서
